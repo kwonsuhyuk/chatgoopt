138a943dd3529999ca132d3ac23b979297ee1a94
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2023-06-10 05:38:23 +0900

    scroll event 등록

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { Box, Container, TextField } from "@mui/material";
+import React, { useCallback, useState } from "react";
+import styled from "styled-components";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    scroll-snap-type: y mandatory;
+    overflow-y: scroll;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+    &::-webkit-scrollbar {
+      width: 0;
+      background-color: transparent;
+    }
+    & > div {
+      position: relative;
+      scroll-snap-align: center;
+      border-top: 1px solid red;
+      height: 100vh;
+    }
+  `;
+
+  const MainDiv = styled.div`
+    background-image: url("https://c.wallhere.com/photos/22/73/sky_clouds_summer-1020592.jpg!d");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  `;
+
+  const handleSubmit = useCallback(async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    const search = data.get("search");
+    window.location.href = `https://www.google.com/search?q=${search}`;
+  }, []);
+
+  return (
+    <Container>
+      <MainDiv></MainDiv>
+      <div>hi</div>
+    </Container>
+  );
 }
 
 export default Dashboard;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,24 +1,40 @@
 import { Box, Button, Paper } from "@mui/material";
-import React from "react";
+import { useEffect, useState } from "react";
 // import { useDispatch, useSelector } from "react-redux";
 import "../firebase";
 import { palette } from "@mui/system";
-import Header from "../components/Header";
 import { Route, Routes, useRoutes } from "react-router";
 import Notfound from "./Notfound";
 import Dashboard from "./Dashboard";
 import Chat from "./Chat";
 import Board from "./Board";
+import Header from "../components/Header";
 
 function Index() {
-  // const { currentUser } = useSelector((state) => state.user);
-  // const name = currentUser?.displayName;
-  // const dispatch = useDispatch();
-  // const router = useRoutes();
+  const [hover, setHover] = useState(false);
 
+  const onMouseEnter = (e) => {
+    e.currentTarget.style.opacity = "1";
+  };
+
+  const onMouseOut = (e) => {
+    e.currentTarget.style.opacity = "0";
+  };
   return (
-    <Box component={Paper} elevation={10} sx={{ color: "primary.main" }}>
-      <Header />
+    <Box component={Paper} elevation={10}>
+      <div
+        onMouseOver={onMouseEnter}
+        onMouseOut={onMouseOut}
+        className={hover ? "show" : "hidden"}
+        style={{
+          position: "fixed",
+          height: "10vh",
+          width: "100%",
+          zIndex: "10",
+          opacity: "0",
+        }}>
+        <Header />
+      </div>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/chat" element={<Chat />} />
